8710c03a8cbc4d08d509f35b662f8a08823f87ea
aleste-dx <eatq.97@gmail.com>
2022-08-29 08:56:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/primeraappmovil/Bienvenido.java
@@ -0,0 +1,19 @@
+package com.example.primeraappmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bienvenido extends AppCompatActivity {
+
+  //  private TextView mensaje;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenido);
+      //  mensaje = findViewById(R.id.textMensaje);
+    }
+  //  protected void saludar(View v)
+}
